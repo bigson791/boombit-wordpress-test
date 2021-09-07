1f55c239d1947cb99991781e9d7c66f710fea019
Elfego Apen <bigson791@gmail.com>
2021-09-07 16:59:36 -0600

    actualizando el thema

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 
-function add_css_js(){
+function boombit_add_css_js(){
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
     wp_enqueue_script('pooper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js', array('jquery'),'2.9',true  );
     wp_enqueue_script('boostrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js', array('pooper'),'5.0',true  );
 }
 
-add_action('wp_enqueue_scripts', 'add_css_js');
+add_action('wp_enqueue_scripts', 'boombit_add_css_js');
 
 // soport images EN featured image support 
 if ( function_exists( 'add_theme_support' ) ) {
@@ -16,3 +16,26 @@ if ( function_exists( 'add_theme_support' ) ) {
  
     add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
  }
+
+ // agregando sidebar
+
+ function boombit_widgets()
+ {
+    register_sidebar(
+        array(
+
+            'id'=>'widget_derecho',
+            'name'=> __( 'widget-Derecha'),
+            'description:'=> __('Arrastra lo que quieras'),
+            'before_widget'=>'<div class="card-body">',
+            'after_widget'=>'</div>',
+            'before_title'=>'<h4>',
+            'after_title'=>'<h4><br>'
+
+
+
+        )
+        );
+ }
+
+ add_action('widgets_init', 'boombit_widgets');
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<div class="col-lg-3">
+                <div class="card-body">
+                    <h4>Publicidad</h4>
+                    <hr>
+                    <img src="/img/vertical.jpg" alt="" class="img-fluid">
+                </div>                  
+            </div>
